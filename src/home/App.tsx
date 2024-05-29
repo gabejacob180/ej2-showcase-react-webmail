bca14c8fa9b8782b73c5fb390864e40d80187344
Gabe <gabe@Gabes-MacBook-Pro.local>
2024-05-29 18:09:09 -0500

    first commit

---
+++ src/home/App.tsx
@@ -0,0 +1,36 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// import { IPages } from '../index';
+// import {
+//     Sidebar, AppBar, TreeView, Toolbar, Accordion, ContextMenu, MenuItemModel, ContextMenuModel, ClickEventArgs,
+//     NodeSelectEventArgs, MenuEventArgs, BeforeOpenCloseMenuEventArgs
+// } from '@syncfusion/ej2-navigations';
+// import { ListView, SelectEventArgs, SortOrder } from '@syncfusion/ej2-lists';
+// import { Button } from '@syncfusion/ej2-buttons';
+// import { AutoComplete, DropDownList, ChangeEventArgs, SelectEventArgs as DropDownSelectEventArgs } from '@syncfusion/ej2-dropdowns';
+// import { Dialog, Popup } from '@syncfusion/ej2-popups';
+// import { Ajax } from '@syncfusion/ej2-base';
+// import { Splitter } from '@syncfusion/ej2-layouts';
+// import { folderData, messageDataSourceNew, getContacts, userName, userMail } from '../datasource';
+// import { showMailDialog, selectedToolbarItem, resetSelectedToolbarItem } from './newmail';
+// import { selectedRPToolbarItem, resetRPSelectedItem, bindReadingPaneData, ddlLastRplyValueRP,
+//     dropdownSelectRP, showMailDialogRP } from '../readingpane';
+import 'styles/css/outlook.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='outlook-container'>
+        <div className='header navbar'>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
